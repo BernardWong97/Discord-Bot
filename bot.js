32b32a2c70cf7a23d709b4403de774ff0bd4b80b
BernardWong97 <ben.wc88@gmail.com>
2021-03-10 16:48:21 +0800

    Daily 6am morning call

--- bot.js
+++ bot.js
@@ -1,21 +1,36 @@
+// Imports
 const Discord = require('discord.js');
+const commandHandler = require('./commands');
+const cron = require('cron');
+require("dotenv").config();
+
+// Instantiate
 const client = new Discord.Client();
 
-require("dotenv").config();
+// Login
 client.login(process.env.TOKEN);
 
+// Terminal debug
 process.stdout.write('Warming up Bok Bok Geh');
-var interval = setInterval(()=>{
+var interval = client.setInterval(()=>{
     process.stdout.write(".");
 }, 500);
 
+// Logged in
 client.on('ready', () => {
+    client.clearInterval(interval);
     process.stdout.write(`\nLogged in as ${client.user.tag}!\n`);
     process.stdout.write('Bok Bok Geh is listening for Discord messages!\n');
-    isReady = true;
-    clearInterval(interval);
+
+    // Daily interval job
+    var cronJob = new cron.CronJob('00 00 06 * * *', wake_up);
+    cronJob.start();
 });
 
-const commandHandler = require('./commands');
-
 client.on('message', commandHandler);
+
+function wake_up(){
+    // CHANGE TEST CHANNEL TO ALL CHAT CHANNEL
+    var channel = client.channels.cache.get(process.env.TESTCHANNEL);
+    channel.send("@everyone BOK BOK BOK WAKE UP!");
+}
--- commands.js
+++ commands.js
@@ -7,37 +7,24 @@ module.exports = async function (message) {
 
     if(message.channel.id == process.env.TESTCHANNEL){
         var splitted = message.content.toLowerCase().split(" ");
+        var command = splitted.shift();
 
-        if(splitted[0] === 'test'){
+        if(command === 'test'){
             message.reply('hello');
         }
 
-        if(splitted[0] === "hello"){
+        if(command === "hello"){
             message.channel.send("Hi! I am a discord bot created by Bernard!");
         }
 
+        // If bok bok geh is mentioned
         if(message.mentions.has(client.user.id)){
             if(message.author.id != process.env.NENGID){
-                var keywords = 'chicken';
-
-                if(splitted.length > 1){
-                    keywords = splitted.slice(1, splitted.length).join(" ");
-                }
-
-                var url = `https://g.tenor.com/v1/search?q=c${keywords}&key=${process.env.TENORKEY}&limit=10`;
-                var response = await fetch(url);
-                var json = await response.json();
-                var index = random_num(json.results.length);
                 
-                message.channel.send(json.results[index].url);
             }
             else{
                 message.channel.send("滾！");
             }
         }
     }
-}
-
-function random_num(max_length){
-    return Math.floor(Math.random() * max_length);
 }
