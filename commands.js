2f728d29d480071ebdb67c52992d30e0d625a22b
BernardWong97 <ben.wc88@gmail.com>
2021-03-11 16:03:15 +0800

    Partially finish mentioning functions

--- commands.js
+++ commands.js
@@ -1,30 +1,30 @@
 const fetch = require('node-fetch');
+const mentions = require('./commands/mentions');
+
+const commands = {
+    mentions,
+    gifs
+}
 
 module.exports = async function (message) {
+    // If message is from bot itself, ignores
     if(message.author.bot) return;
 
+    // If message is mentioning everyone, ignores
     if(message.content.includes("@here") || message.content.includes("@everyone")) return;
 
+    // If message is test channel
     if(message.channel.id == process.env.TESTCHANNEL){
+        // Save each incoming message words into the element of splitted variable.
         var splitted = message.content.toLowerCase().split(" ");
-        var command = splitted.shift();
 
-        if(command === 'test'){
-            message.reply('hello');
-        }
+        // Remove the first element
+        var id = splitted.shift();
 
-        if(command === "hello"){
-            message.channel.send("Hi! I am a discord bot created by Bernard!");
-        }
+        mentions(message, splitted);
 
-        // If bok bok geh is mentioned
-        if(message.mentions.has(client.user.id)){
-            if(message.author.id != process.env.NENGID){
-                
-            }
-            else{
-                message.channel.send("滾！");
-            }
-        }
+        // Execute commands
+        var command = "gifs";
+        commands[command](message, splitted);
     }
 }
--- commands/mentions.js
+++ commands/mentions.js
@@ -1,22 +1,50 @@
-var nengQuotes = ["test", "test 2", "different test"];
+module.exports = function(message, splitted) {
+    // Quotes
+    var selfQuotes = ["Why do you tag me?", "什麽事情啊？親？", "叫我嗎, 寶貝?", "😘", "❤️"];
+    var shooQuotes = ["滾!", "走開", "不要叫我", "打擾貴人吃玉米你想被啄嗎？", "💩"];
+    var berdQuotes = ["叫我大哥幹嘛？", "不見去", "把我電子化的人"];
+    var nengQuotes = ["叫我的僕人做莫？", "每天幫我梳毛的人", "Ewww"];
+    var shanniQuotes = ["Reply please", "Do your job, I need electricity to go online"];
 
-var quotes = {
-    mentionBerd: "berdQuotes",
-    mentionNeng: nengQuotes,
-    mentionShen: "shenQuotes"
+    // Libraries
+    var quotes = {
+        mentionBot: selfQuotes,
+        mentionBotNeng: shooQuotes,
+        mentionBerd: berdQuotes,
+        mentionNeng: nengQuotes,
+        mentionShanni: shanniQuotes
+    }
+
+    // If message only mention bot without any more words
+    if(message.mentions.has(process.env.BOTID)){
+        if(message.author.id != process.env.NENGID){
+            sendQuote(quotes["mentionBot"]);
+        }
+        else{
+            sendQuote(quotes["mentionBotNeng"]);
+        }
+
+    }
+
+    // Mention Berd
+    userMention(process.env.BERDID, "mentionBerd");
+
+    // Mention Neng
+    userMention(process.env.NENGID, "mentionNeng");
+
+    // Mention Shen
+    userMention(process.env.SHANNIID, "mentionShanni");
+
+    function userMention(id, quoteKey){
+        if(message.mentions.has(id)){
+            sendQuote(quotes[quoteKey]);
+        }
+    }
+
+    function sendQuote(arr){
+        // Select a random quote from library and send
+        var random = Math.floor(Math.random() * arr.length);
+        var quote = arr[random];
+        message.channel.send(quote);
+    }
 }
-
-display(quotes["mentionNeng"]);
-
-function display(arr){
-    var random = Math.floor(Math.random() * arr.length);
-    var quote = arr[random]
-    console.log(quote)
-}
-
-// Mention Berd
-
-// Mention Neng
-
-// Mention Shen
-
